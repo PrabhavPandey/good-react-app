7ca74e4e25b62af2b3554dc7cc4aec917fd276e3
Prabhav <prabhavp380@gmail.com>
2022-06-27 00:06:06 +0530

    cra is the best xD

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>yo</h1>
-      <p>this is a react app <br /> its clean <br />(no tests and webvitals). <br />its fast. <br /> its responsive.</p>
+      <p>this is a react app. <br /> its clean. <br />its fast. <br /> its responsive.</p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/HyWYpM_S-2c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <p>built with ❤️ by <a href="https://github.com/PrabhavPandey" target="_blank">Prabhav Pandey</a></p>
     </div>
   );
 }

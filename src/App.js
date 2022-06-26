3abe54cf740afeea2d91a231f6c0a10394a0bd63
Prabhav <prabhavp380@gmail.com>
2022-06-26 21:55:39 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>yo</h1>
+      <p>this is a react app <br /> its clean <br />(no tests and webvitals). <br />its fast. <br /> its responsive.</p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/HyWYpM_S-2c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  );
+}
+
+export default App;
